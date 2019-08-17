3dfe945a3aefff615a2afb19a47b76f0e3d72caa
OverMighty <its.overmighty@gmail.com>
2019-08-17 20:22:07 +0200

    feat(ScrollableGUI): add {page} macro for the display name of items used for navigation buttons

--- src/main/java/com/github/overmighty/croissant/gui/ScrollableGUI.java
+++ src/main/java/com/github/overmighty/croissant/gui/ScrollableGUI.java
@@ -3,6 +3,7 @@ package com.github.overmighty.croissant.gui;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,9 +66,21 @@ public class ScrollableGUI {
         }
     }
 
+    private ItemStack makeNavigationItem(ItemStack item, int pageIndex) {
+        ItemStack navigationItem = item.clone();
+        ItemMeta itemMeta = navigationItem.getItemMeta();
+        itemMeta.setDisplayName(
+            itemMeta.getDisplayName().replace("{page}", Integer.toString(pageIndex + 1))
+        );
+        navigationItem.setItemMeta(itemMeta);
+        return navigationItem;
+    }
+
     /**
      * Sets a navigation button on the GUI, to allow players to scroll between
-     * the GUI's pages.
+     * the GUI's pages. For each page of the GUI, {@code {page}} in the item
+     * stack's display name will be replaced with the number of the page that
+     * the button will take the player to.
      *
      * @param slot the slot of each page in which to set the button
      * @param item the item stack to use to display the button
@@ -76,27 +89,31 @@ public class ScrollableGUI {
     public void setNavigationButton(int slot, ItemStack item, NavigationButtonType type) {
         if (type == NavigationButtonType.FIRST_PAGE) {
             for (int i = 2; i < this.pages.size(); i++) {
-                this.pages.get(i).setButton(slot, item, event ->
+                ItemStack navigationItem = this.makeNavigationItem(item, 0);
+                this.pages.get(i).setButton(slot, navigationItem, event ->
                     this.pages.get(0).openTo((Player) event.getWhoClicked())
                 );
             }
         } else if (type == NavigationButtonType.PREVIOUS_PAGE) {
             for (int i = 1; i < this.pages.size(); i++) {
                 int previousPageIndex = i - 1;
-                this.pages.get(i).setButton(slot, item, event ->
+                ItemStack navigationItem = this.makeNavigationItem(item, previousPageIndex);
+                this.pages.get(i).setButton(slot, navigationItem, event ->
                     this.pages.get(previousPageIndex).openTo((Player) event.getWhoClicked())
                 );
             }
         } else if (type == NavigationButtonType.NEXT_PAGE) {
             for (int i = 0; i < this.pages.size() - 1; i++) {
                 int nextPageIndex = i + 1;
-                this.pages.get(i).setButton(slot, item, event ->
+                ItemStack navigationItem = this.makeNavigationItem(item, nextPageIndex);
+                this.pages.get(i).setButton(slot, navigationItem, event ->
                     this.pages.get(nextPageIndex).openTo((Player) event.getWhoClicked())
                 );
             }
         } else if (type == NavigationButtonType.LAST_PAGE) {
             for (int i = 0; i < this.pages.size() - 2; i++) {
-                this.pages.get(i).setButton(slot, item, event ->
+                ItemStack navigationItem = this.makeNavigationItem(item, this.pages.size() - 1);
+                this.pages.get(i).setButton(slot, navigationItem, event ->
                     this.pages.get(this.pages.size() - 1).openTo((Player) event.getWhoClicked())
                 );
             }
